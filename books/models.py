7944c5e9403fd15c238df75faf0df48542b359ef
hebel-dev <hebel.dev@gmail.com>
2021-08-01 20:25:21 +0200

    Create Book class

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.deletion import CASCADE
 
 class Author (models.Model):
     name_author = models.CharField(max_length=20)
@@ -9,5 +10,10 @@ class Author (models.Model):
     def __str__(self):
         return f" Autor\ka {self.name_author} {self.surname_author}, rok urodzenia: {self.year_of_birth_autor}"
 
+class Book (models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    title_book = models.CharField(max_length=100)
+    year_of_publishment = models.IntegerField(validators=[MaxValueValidator(2100), MinValueValidator(-2100)])
 
+    
 # Create your models here.
