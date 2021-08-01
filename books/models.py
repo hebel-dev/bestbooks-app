2d345174e0b740833737241c00be908c4dc945c9
hebel-dev <hebel.dev@gmail.com>
2021-08-01 20:16:09 +0200

    Create str method in Author class

--- books/models.py
+++ books/models.py
@@ -6,6 +6,8 @@ class Author (models.Model):
     surname_author = models.CharField(max_length=40)
     year_of_birth_autor = models.IntegerField(validators=[MaxValueValidator(2100), MinValueValidator(-2100)])
 
+    def __str__(self):
+        return f" Autor\ka {self.name_author} {self.surname_author}, rok urodzenia: {self.year_of_birth_autor}"
 
 
 # Create your models here.
