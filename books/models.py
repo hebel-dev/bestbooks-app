58f4868d89dae04c352bedb2025fc11f9721699a
hebel-dev <hebel.dev@gmail.com>
2021-08-01 17:58:29 +0200

    Create class Author

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Author (models.Model):
+    name_author = models.CharField(max_length=20)
+    surname_author = models.CharField(max_length=40)
+    year_of_birth_autor = models.IntegerField(validators=[MaxValueValidator(2100), MinValueValidator(-2100)])
+
+
 
 # Create your models here.
