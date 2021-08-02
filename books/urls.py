d3baa950c1b6d99c22707e485feb4f769449a437
hebel-dev <hebel.dev@gmail.com>
2021-08-02 16:48:01 +0200

    :Create two views :all author and all books of author

--- books/urls.py
+++ books/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('autors/', views.authors_without_books, name='authors_without_books'),
+    path('<int:author_id>/books', views.author_with_all_books, name='author_with_all_books')
+
+    
 
 ]
 
--- books/views.py
+++ books/views.py
@@ -1,8 +1,26 @@
+import books
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
-
+from .models import Author, Book
 
 def index(request):
-    return HttpResponse('Hello  world')
+    three_author_list = Author.objects.order_by('-surname_author')[:3]
+    txt = ''
+    for author in three_author_list:
+        txt += author.surname_author +' '+ author.name_author +'<br>'
+    return HttpResponse(f'{txt}')
+
+def authors_without_books(request):
+    authors = Author.objects.all()
+    txt = ''
+    for author in authors:
+        txt += author.surname_author + ' ' + author.name_author + '<br>'
+    return HttpResponse(f'lista autorów: <br> {txt}')
+
+def author_with_all_books(request, author_id):
+    author = Author.objects.get(id=author_id)
+    all_books = Book.objects.all()
+    txt = ''
+    for book in all_books :
+        txt += book.title_book + ' '
+    return HttpResponse(f'hej oto nasz twórca : {author} <br> a o to lista jego utworów : <br> {txt}' )    
