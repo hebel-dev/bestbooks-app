9a924bc2bff147d094d1187dcd59ddfb4d9ff3d2
hebel-dev <hebel.dev@gmail.com>
2021-08-16 10:00:55 +0200

    Register BookComment class in admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Author, Book, Coment
+from .models import Author, Book, BookComment, Coment
 
 admin.site.register(Author)
 admin.site.register(Book)
-admin.site.register(Coment)
+admin.site.register(BookComment)
 
 # Register your models here.
